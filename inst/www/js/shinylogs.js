17acb80eba4833b79b03a43017b65f542773bfab
pvictor <perrier.victor@gmail.com>
2018-12-20 11:09:02 +0100

    expose browser info

--- inst/www/js/shinylogs.js
+++ inst/www/js/shinylogs.js
@@ -6,126 +6,155 @@
  *
  * @version 0.0.1
  */
+// on unload or not
+var logsonunload = false;
 
+// config
+var config = document.querySelectorAll('script[data-for="shinylogs"]');
 
-  // on unload or not
-  var logsonunload = false;
+config = JSON.parse(config[0].innerHTML);
 
-  // config
-  var config = document.querySelectorAll('script[data-for="shinylogs"]');
-  config = JSON.parse(config[0].innerHTML);
-  console.log(config);
-  logsonunload = config.logsonunload;
-  //console.log(logsonunload);
+console.log(config);
 
-  var re_ex_in = RegExp("^$");
-  if (config.hasOwnProperty("excludeinput")) {
-    re_ex_in = RegExp(config.excludeinput);
-  }
+logsonunload = config.logsonunload;
 
+//console.log(logsonunload);
+var re_ex_in = RegExp("^$");
 
-  // lowdb init
-  var adapter = new LocalStorage('db');
-  var db = low(adapter);
+if (config.hasOwnProperty("excludeinput")) {
+  re_ex_in = RegExp(config.excludeinput);
+}
 
-  // initialize local data storage
-  db.defaults({ input: [], error: [], output: [] }).write();
+// lowdb init
+var adapter = new LocalStorage("db");
 
-  // User Agent
-  var ua = window.navigator.userAgent;
+var db = low(adapter);
 
-  // Screen resolution
-  var screen_res = window.screen.width + "x" + window.screen.height;
-  // Browser resolution
-  var w = window.innerWidth
-            || document.documentElement.clientWidth
-            || document.body.clientWidth;
-  var h = window.innerHeight
-            || document.documentElement.clientHeight
-            || document.body.clientHeight;
-  var browser_res = w + "x" + h;
-  // Pixel ratio
-  var pixel_ratio = window.devicePixelRatio;
+// initialize local data storage
+db.defaults({
+  input: [],
+  error: [],
+  output: []
+}).write();
 
-  // Shiny input event to not track
-  var dont_track = [".shinylogs_lastinput", ".shinylogs_input", ".shinylogs_error", ".shinylogs_output"];
-  var regex_hidden = RegExp('hidden$');
+// User Agent
+var ua = window.navigator.userAgent;
 
-  // Track INPUTS
-  $(document).on('shiny:inputchanged', function(event) {
+// Screen resolution
+var screen_res = window.screen.width + "x" + window.screen.height;
+
+// Browser resolution
+var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+var browser_res = w + "x" + h;
+
+// Pixel ratio
+var pixel_ratio = window.devicePixelRatio;
+
+// Send browser data
+Shiny.setInputValue(".shinylogs_browserData", {
+  user_agent: ua,
+  screen_res: screen_res,
+  browser_res: browser_res,
+  pixel_ratio: pixel_ratio
+});
+
+// Shiny input event to not track
+var dont_track = [ ".shinylogs_lastinput", ".shinylogs_input", ".shinylogs_error", ".shinylogs_output" ];
+
+var regex_hidden = RegExp("hidden$");
+
+// Track INPUTS
+$(document).on("shiny:inputchanged", function(event) {
+  //console.log(event);
+  if (dont_track.indexOf(event.name) == -1 & regex_hidden.test(event.name) === false & re_ex_in.test(event.name) === false) {
     //console.log(event);
-    if (dont_track.indexOf(event.name) == -1 & regex_hidden.test(event.name) === false & re_ex_in.test(event.name) === false) {
-      //console.log(event);
-      var lastInput = {name: event.name, timestamp: event.timeStamp, value: event.value, type: event.inputType};
-      db.get('input').push(lastInput).write();
-      if (logsonunload === false) {
-        Shiny.setInputValue(".shinylogs_lastinput", lastInput, {priority: "event"});
-        var input_ = db.get('input').value();
-        input_ = JSON.stringify(input_);
-        Shiny.setInputValue(".shinylogs_input:parse_log", {inputs: input_}, {priority: "event"});
-      }
-    }
-  });
-
-  // Track ERRORS
-  $(document).on("shiny:error", function(event) {
-    //console.log(event);
-    if (dont_track.indexOf(event.name) == -1) {
-      var lastError = {name: event.name, timestamp: event.timeStamp, error: event.error.message};
-      db.get('error').push(lastError).write();
-      if (logsonunload === false) {
-        var error_ = db.get('error').value();
-        error_ = JSON.stringify(error_);
-        Shiny.setInputValue(".shinylogs_error:parse_log", {errors: error_});
-      }
-    }
-  });
-
-  // Track OUTPUTs
-  $(document).on("shiny:value", function(event) {
-    //console.log(event);
-    var lastOutput = {name: event.name, timestamp: event.timeStamp, binding: event.binding.binding.name};
-    db.get('output').push(lastOutput).write();
-    if (logsonunload === false) {
-      var output_ = db.get('output').value();
-      output_ = JSON.stringify(output_);
-      Shiny.setInputValue(".shinylogs_output:parse_log", {outputs: output_});
-    }
-  });
-
-
-  if (logsonunload === true) {
-    window.onbeforeunload = function(e) {
-
-      var e = e || window.event;
-
-      // For IE and Firefox
-      if (e) {
-        e.returnValue = "Are you sure?";
-      }
-
-      var input_ = db.get('input').value();
-      input_ = JSON.stringify(input_);
-      Shiny.setInputValue(".shinylogs_input:parse_log", {inputs: input_}, {priority: "event"});
-
-      var error_ = db.get('error').value();
-      error_ = JSON.stringify(error_);
-      Shiny.setInputValue(".shinylogs_error:parse_log", {errors: error_});
-
-      var output_ = db.get('output').value();
-      output_ = JSON.stringify(output_);
-      Shiny.setInputValue(".shinylogs_output:parse_log", {outputs: output_});
-
-      return "Are you sure?";
+    var lastInput = {
+      name: event.name,
+      timestamp: event.timeStamp,
+      value: event.value,
+      type: event.inputType
     };
+    db.get("input").push(lastInput).write();
+    if (logsonunload === false) {
+      Shiny.setInputValue(".shinylogs_lastinput", lastInput, {
+        priority: "event"
+      });
+      var input_ = db.get("input").value();
+      input_ = JSON.stringify(input_);
+      Shiny.setInputValue(".shinylogs_input:parse_log", {
+        inputs: input_
+      }, {
+        priority: "event"
+      });
+    }
   }
+});
 
+// Track ERRORS
+$(document).on("shiny:error", function(event) {
+  //console.log(event);
+  if (dont_track.indexOf(event.name) == -1) {
+    var lastError = {
+      name: event.name,
+      timestamp: event.timeStamp,
+      error: event.error.message
+    };
+    db.get("error").push(lastError).write();
+    if (logsonunload === false) {
+      var error_ = db.get("error").value();
+      error_ = JSON.stringify(error_);
+      Shiny.setInputValue(".shinylogs_error:parse_log", {
+        errors: error_
+      });
+    }
+  }
+});
 
+// Track OUTPUTs
+$(document).on("shiny:value", function(event) {
+  //console.log(event);
+  var lastOutput = {
+    name: event.name,
+    timestamp: event.timeStamp,
+    binding: event.binding.binding.name
+  };
+  db.get("output").push(lastOutput).write();
+  if (logsonunload === false) {
+    var output_ = db.get("output").value();
+    output_ = JSON.stringify(output_);
+    Shiny.setInputValue(".shinylogs_output:parse_log", {
+      outputs: output_
+    });
+  }
+});
 
-
-
-
-
-
-
-
+if (logsonunload === true) {
+  window.onbeforeunload = function(e) {
+    var e = e || window.event;
+    // For IE and Firefox
+    if (e) {
+      e.returnValue = "Are you sure?";
+    }
+    var input_ = db.get("input").value();
+    input_ = JSON.stringify(input_);
+    Shiny.setInputValue(".shinylogs_input:parse_log", {
+      inputs: input_
+    }, {
+      priority: "event"
+    });
+    var error_ = db.get("error").value();
+    error_ = JSON.stringify(error_);
+    Shiny.setInputValue(".shinylogs_error:parse_log", {
+      errors: error_
+    });
+    var output_ = db.get("output").value();
+    output_ = JSON.stringify(output_);
+    Shiny.setInputValue(".shinylogs_output:parse_log", {
+      outputs: output_
+    });
+    return "Are you sure?";
+  };
+}
