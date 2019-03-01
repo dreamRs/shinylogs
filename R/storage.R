fffd52b5c6d799caea0bf7dcb901e1f7ee73e700
pvictor <perrier.victor@gmail.com>
2019-03-01 10:52:54 +0100

    storage methods

--- R/storage.R
+++ R/storage.R
@@ -18,9 +18,45 @@ store_json <- function(path = "shinylogs") {
 }
 
 
+#' No storage on disk
+#'
+#' @export
+#'
+store_null <- function() {
+  store <- list(
+    mode = "null"
+  )
+  class(store) <- c(class(store), "shinylogs.storage_mode")
+  return(store)
+}
+
+
+#' Use RDS files as storage mode
+#'
+#' @param path  Path where to write RDS files.
+#'
+#' @export
+#'
+store_rds <- function(path = "shinylogs") {
+  path <- normalizePath(path = path, mustWork = FALSE)
+  if (!dir.exists(path))
+    dir.create(path)
+  store <- list(
+    mode = "rds",
+    path = path
+  )
+  class(store) <- c(class(store), "shinylogs.storage_mode")
+  return(store)
+}
+
+
 write_logs <- function(opts, logs) {
   if (opts$mode == "json") {
     write_logs_json(opts, logs)
+  } else if (opts$mode == "rds") {
+    write_logs_rds(opts, logs)
+  } else if (opts$mode == "null") {
+    invisible()
   } else {
     stop("Not implemented!", call. = FALSE)
   }
@@ -35,4 +71,10 @@ write_logs_json <- function(opts, logs) {
   jsonlite::write_json(x = logs, path = path, auto_unbox = TRUE)
 }
 
-
+write_logs_rds <- function(opts, logs) {
+  path <- file.path(
+    opts$path,
+    paste0("shinylogs_", opts$appname, "_", opts$timestamp, ".rds")
+  )
+  saveRDS(object = logs, file = path)
+}
