7687ec579d896132ba263ce3fdb5c811d824e65e
pvictor <perrier.victor@gmail.com>
2019-04-07 14:45:30 +0200

    sqlite storage

--- R/storage.R
+++ R/storage.R
@@ -18,6 +18,7 @@ store_json <- function(path = "shinylogs") {
 }
 
 
+
 #' No storage on disk
 #'
 #' @export
@@ -31,9 +32,10 @@ store_null <- function() {
 }
 
 
+
 #' Use RDS files as storage mode
 #'
-#' @param path  Path where to write RDS files.
+#' @param path Path where to write RDS files.
 #'
 #' @export
 #'
@@ -50,6 +52,32 @@ store_rds <- function(path = "shinylogs") {
 }
 
 
+
+#' Use SQLite database as storage mode
+#'
+#' @param path Path to the SQLite file or a directory where to create one.
+#'
+#' @export
+#'
+store_sqlite <- function(path = "shinylogs") {
+  path <- normalizePath(path = path, mustWork = FALSE)
+  if (is_sqlite(path)) {
+    if (!dir.exists(dirname(path)))
+      dir.create(dirname(path))
+  } else {
+    if (!dir.exists(path))
+      dir.create(path)
+    path <- file.path(path, "shinylogs.sqlite")
+  }
+  store <- list(
+    mode = "sqlite",
+    path = normalizePath(path = path, mustWork = FALSE)
+  )
+  class(store) <- c(class(store), "shinylogs.storage_mode")
+  return(store)
+}
+
+
 write_logs <- function(opts, logs) {
   if (opts$mode == "json") {
     write_logs_json(opts, logs)
@@ -57,6 +85,8 @@ write_logs <- function(opts, logs) {
     write_logs_rds(opts, logs)
   } else if (opts$mode == "null") {
     invisible()
+  } else if (opts$mode == "sqlite") {
+    write_logs_sqlite(opts, logs)
   } else {
     stop("Not implemented!", call. = FALSE)
   }
@@ -78,3 +108,32 @@ write_logs_rds <- function(opts, logs) {
   )
   saveRDS(object = logs, file = path)
 }
+
+
+#' @importFrom DBI dbConnect dbDisconnect dbWriteTable
+#' @importFrom RSQLite SQLite
+write_logs_sqlite <- function(opts, logs) {
+  con <- DBI::dbConnect(drv = RSQLite::SQLite(), dbname = opts$path)
+  on.exit(DBI::dbDisconnect(conn = con))
+  lapply(
+    X = names(logs),
+    FUN = function(what) {
+      value <- extract_dt(logs, what)
+      if (!is.null(value)) {
+        value <- value[, lapply(.SD, function(x) {
+          if (is.list(x)) {
+            x <- I(lapply(x, serialize, connection = NULL))
+          }
+          x
+        })]
+        value <- as.data.frame(value)
+        value$sessionid <- logs$session$sessionid
+        DBI::dbWriteTable(conn = con, name = what, value = value, append = TRUE)
+      }
+    }
+  )
+  invisible()
+}
+
+
+
--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,7 @@ get_timestamp <- function(time = NULL) {
   format(time, format = "%Y-%m-%dT%H:%M:%S%z")
 }
 
+is_sqlite <- function(path) {
+  is.character(path) && grepl(pattern = "\\.sqlite$", x = path)
+}
+
